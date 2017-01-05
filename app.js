25a48a3fa5e2d6b05cb86b850c4da810a1c95a67
Vishal Agrawal <vishal.agrawal@in.ibm.com>
2017-01-05 03:17:59 -0600

    commit changes

--- app.js
+++ app.js
@@ -1,427 +1,149 @@
-/**
- * Module dependencies.
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var express = require('express'),
-    routes = require('./routes'),
-    user = require('./routes/user'),
-    http = require('http'),
-    path = require('path'),
-    fs = require('fs');
+var routes = require('./routes/index');
+var users = require('./routes/user');
 
 var app = express();
+var ws = require('ws');		
+var WebSocket = require('ws/lib/WebSocket')
 
-var db;
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env === 'development';
 
-var cloudant;
+// view engine setup
 
-var fileToUpload;
-
-var dbCredentials = {
-    dbName: 'my_sample_db'
-};
-
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var logger = require('morgan');
-var errorHandler = require('errorhandler');
-var multipart = require('connect-multiparty')
-var multipartMiddleware = multipart();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+/*app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');*/
+
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(bodyParser.json());
-app.use(methodOverride());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/style', express.static(path.join(__dirname, '/views/style')));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(errorHandler());
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
+    });
 }
 
-function initDBConnection() {
-    //When running on Bluemix, this variable will be set to a json object
-    //containing all the service credentials of all the bound services
-    if (process.env.VCAP_SERVICES) {
-        var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-        // Pattern match to find the first instance of a Cloudant service in
-        // VCAP_SERVICES. If you know your service key, you can access the
-        // service credentials directly by using the vcapServices object.
-        for (var vcapService in vcapServices) {
-            if (vcapService.match(/cloudant/i)) {
-                dbCredentials.url = vcapServices[vcapService][0].credentials.url;
-            }
-        }
-    } else { //When running locally, the VCAP_SERVICES will not be set
-
-        // When running this app locally you can get your Cloudant credentials
-        // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
-        // Variables section for an app in the Bluemix console dashboard).
-        // Alternately you could point to a local database here instead of a
-        // Bluemix service.
-        // url will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
-        dbCredentials.url = "REPLACE ME";
-    }
-
-    cloudant = require('cloudant')(dbCredentials.url);
-
-    // check if DB exists if not create
-    cloudant.db.create(dbCredentials.dbName, function(err, res) {
-        if (err) {
-            console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
-        }
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
     });
+});
 
-    db = cloudant.use(dbCredentials.dbName);
+/*connect_to_server();
+
+
+//=================================================================================
+//Socket Stuff
+//=================================================================================
+function connect_to_server(){
+	var connected = false;
+	connect();
+	
+	function connect(){
+		//var wsUri = 'ws://' + document.location.hostname + ':' + document.location.port;
+		var wsUri = 'ws://' + 'localhost' + ':' + '3001';
+		console.log('Connectiong to websocket', wsUri);
+		
+		ws = new WebSocket(wsUri);
+		ws.onopen = function(evt) { onOpen(evt); };
+		ws.onclose = function(evt) { onClose(evt); };
+		ws.onmessage = function(evt) { onMessage(evt); };
+		ws.onerror = function(evt) { onError(evt); };
+	}
+	
+	function onOpen(evt){
+		console.log('WS CONNECTED');
+		connected = true;
+		//clear_blocks();
+		//$('#errorNotificationPanel').fadeOut();
+		ws.send(JSON.stringify({type: 'get', v:1}));
+		//ws.send(JSON.stringify({type: 'chainstats', v:1}));
+	}
+
+	function onClose(evt){
+		console.log('WS DISCONNECTED', evt);
+		connected = false;
+		setTimeout(function(){ connect(); }, 5000);					//try again one more time, server restarts are quick
+	}
+
+	function onMessage(msg){
+		try{
+			var msgObj = JSON.parse(msg.data);
+			if(msgObj.marble){
+				console.log('rec', msgObj.msg, msgObj);
+				//build_ball(msgObj.marble);
+			}
+			else if(msgObj.msg === 'chainstats'){
+				console.log('rec', msgObj.msg, ': ledger blockheight', msgObj.chainstats.height, 'block', msgObj.blockstats.height);
+				var e = formatDate(msgObj.blockstats.transactions[0].timestamp.seconds * 1000, '%M/%d/%Y &nbsp;%I:%m%P');
+				$('#blockdate').html('<span style="color:#fff">TIME</span>&nbsp;&nbsp;' + e + ' UTC');
+				var temp =  {
+								id: msgObj.blockstats.height, 
+								blockstats: msgObj.blockstats
+							};
+				new_block(temp);								//send to blockchain.js
+			}
+			else console.log('rec', msgObj.msg, msgObj);
+		}
+		catch(e){
+			console.log('ERROR', e);
+		}
+	}
+
+	function onError(evt){
+		console.log('ERROR ', evt);
+		if(!connected && bag.e == null){											//don't overwrite an error message
+			$('#errorName').html('Warning');
+			$('#errorNoticeText').html('Waiting on the node server to open up so we can talk to the blockchain. ');
+			$('#errorNoticeText').append('This app is likely still starting up. ');
+			$('#errorNoticeText').append('Check the server logs if this message does not go away in 1 minute. ');
+			$('#errorNotificationPanel').fadeIn();
+		}
+	}
 }
-
-initDBConnection();
-
-app.get('/', routes.index);
-
-function createResponseData(id, name, value, attachments) {
-
-    var responseData = {
-        id: id,
-        name: name,
-        value: value,
-        attachements: []
-    };
-
-
-    attachments.forEach(function(item, index) {
-        var attachmentData = {
-            content_type: item.type,
-            key: item.key,
-            url: '/api/favorites/attach?id=' + id + '&key=' + item.key
-        };
-        responseData.attachements.push(attachmentData);
-
-    });
-    return responseData;
+function get_ws(){
+	return ws;
 }
-
-
-var saveDocument = function(id, name, value, response) {
-
-    if (id === undefined) {
-        // Generated random id
-        id = '';
-    }
-
-    db.insert({
-        name: name,
-        value: value
-    }, id, function(err, doc) {
-        if (err) {
-            console.log(err);
-            response.sendStatus(500);
-        } else
-            response.sendStatus(200);
-        response.end();
-    });
-
-}
-
-app.get('/api/favorites/attach', function(request, response) {
-    var doc = request.query.id;
-    var key = request.query.key;
-
-    db.attachment.get(doc, key, function(err, body) {
-        if (err) {
-            response.status(500);
-            response.setHeader('Content-Type', 'text/plain');
-            response.write('Error: ' + err);
-            response.end();
-            return;
-        }
-
-        response.status(200);
-        response.setHeader("Content-Disposition", 'inline; filename="' + key + '"');
-        response.write(body);
-        response.end();
-        return;
-    });
-});
-
-app.post('/api/favorites/attach', multipartMiddleware, function(request, response) {
-
-    console.log("Upload File Invoked..");
-    console.log('Request: ' + JSON.stringify(request.headers));
-
-    var id;
-
-    db.get(request.query.id, function(err, existingdoc) {
-
-        var isExistingDoc = false;
-        if (!existingdoc) {
-            id = '-1';
-        } else {
-            id = existingdoc.id;
-            isExistingDoc = true;
-        }
-
-        var name = request.query.name;
-        var value = request.query.value;
-
-        var file = request.files.file;
-        var newPath = './public/uploads/' + file.name;
-
-        var insertAttachment = function(file, id, rev, name, value, response) {
-
-            fs.readFile(file.path, function(err, data) {
-                if (!err) {
-
-                    if (file) {
-
-                        db.attachment.insert(id, file.name, data, file.type, {
-                            rev: rev
-                        }, function(err, document) {
-                            if (!err) {
-                                console.log('Attachment saved successfully.. ');
-
-                                db.get(document.id, function(err, doc) {
-                                    console.log('Attachements from server --> ' + JSON.stringify(doc._attachments));
-
-                                    var attachements = [];
-                                    var attachData;
-                                    for (var attachment in doc._attachments) {
-                                        if (attachment == value) {
-                                            attachData = {
-                                                "key": attachment,
-                                                "type": file.type
-                                            };
-                                        } else {
-                                            attachData = {
-                                                "key": attachment,
-                                                "type": doc._attachments[attachment]['content_type']
-                                            };
-                                        }
-                                        attachements.push(attachData);
-                                    }
-                                    var responseData = createResponseData(
-                                        id,
-                                        name,
-                                        value,
-                                        attachements);
-                                    console.log('Response after attachment: \n' + JSON.stringify(responseData));
-                                    response.write(JSON.stringify(responseData));
-                                    response.end();
-                                    return;
-                                });
-                            } else {
-                                console.log(err);
-                            }
-                        });
-                    }
-                }
-            });
-        }
-
-        if (!isExistingDoc) {
-            existingdoc = {
-                name: name,
-                value: value,
-                create_date: new Date()
-            };
-
-            // save doc
-            db.insert({
-                name: name,
-                value: value
-            }, '', function(err, doc) {
-                if (err) {
-                    console.log(err);
-                } else {
-
-                    existingdoc = doc;
-                    console.log("New doc created ..");
-                    console.log(existingdoc);
-                    insertAttachment(file, existingdoc.id, existingdoc.rev, name, value, response);
-
-                }
-            });
-
-        } else {
-            console.log('Adding attachment to existing doc.');
-            console.log(existingdoc);
-            insertAttachment(file, existingdoc._id, existingdoc._rev, name, value, response);
-        }
-
-    });
-
-});
-
-app.post('/api/favorites', function(request, response) {
-
-    console.log("Create Invoked..");
-    console.log("Name: " + request.body.name);
-    console.log("Value: " + request.body.value);
-
-    // var id = request.body.id;
-    var name = request.body.name;
-    var value = request.body.value;
-
-    saveDocument(null, name, value, response);
-
-});
-
-app.delete('/api/favorites', function(request, response) {
-
-    console.log("Delete Invoked..");
-    var id = request.query.id;
-    // var rev = request.query.rev; // Rev can be fetched from request. if
-    // needed, send the rev from client
-    console.log("Removing document of ID: " + id);
-    console.log('Request Query: ' + JSON.stringify(request.query));
-
-    db.get(id, {
-        revs_info: true
-    }, function(err, doc) {
-        if (!err) {
-            db.destroy(doc._id, doc._rev, function(err, res) {
-                // Handle response
-                if (err) {
-                    console.log(err);
-                    response.sendStatus(500);
-                } else {
-                    response.sendStatus(200);
-                }
-            });
-        }
-    });
-
-});
-
-app.put('/api/favorites', function(request, response) {
-
-    console.log("Update Invoked..");
-
-    var id = request.body.id;
-    var name = request.body.name;
-    var value = request.body.value;
-
-    console.log("ID: " + id);
-
-    db.get(id, {
-        revs_info: true
-    }, function(err, doc) {
-        if (!err) {
-            console.log(doc);
-            doc.name = name;
-            doc.value = value;
-            db.insert(doc, doc.id, function(err, doc) {
-                if (err) {
-                    console.log('Error inserting data\n' + err);
-                    return 500;
-                }
-                return 200;
-            });
-        }
-    });
-});
-
-app.get('/api/favorites', function(request, response) {
-
-    console.log("Get method invoked.. ")
-
-    db = cloudant.use(dbCredentials.dbName);
-    var docList = [];
-    var i = 0;
-    db.list(function(err, body) {
-        if (!err) {
-            var len = body.rows.length;
-            console.log('total # of docs -> ' + len);
-            if (len == 0) {
-                // push sample data
-                // save doc
-                var docName = 'sample_doc';
-                var docDesc = 'A sample Document';
-                db.insert({
-                    name: docName,
-                    value: 'A sample Document'
-                }, '', function(err, doc) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-
-                        console.log('Document : ' + JSON.stringify(doc));
-                        var responseData = createResponseData(
-                            doc.id,
-                            docName,
-                            docDesc, []);
-                        docList.push(responseData);
-                        response.write(JSON.stringify(docList));
-                        console.log(JSON.stringify(docList));
-                        console.log('ending response...');
-                        response.end();
-                    }
-                });
-            } else {
-
-                body.rows.forEach(function(document) {
-
-                    db.get(document.id, {
-                        revs_info: true
-                    }, function(err, doc) {
-                        if (!err) {
-                            if (doc['_attachments']) {
-
-                                var attachments = [];
-                                for (var attribute in doc['_attachments']) {
-
-                                    if (doc['_attachments'][attribute] && doc['_attachments'][attribute]['content_type']) {
-                                        attachments.push({
-                                            "key": attribute,
-                                            "type": doc['_attachments'][attribute]['content_type']
-                                        });
-                                    }
-                                    console.log(attribute + ": " + JSON.stringify(doc['_attachments'][attribute]));
-                                }
-                                var responseData = createResponseData(
-                                    doc._id,
-                                    doc.name,
-                                    doc.value,
-                                    attachments);
-
-                            } else {
-                                var responseData = createResponseData(
-                                    doc._id,
-                                    doc.name,
-                                    doc.value, []);
-                            }
-
-                            docList.push(responseData);
-                            i++;
-                            if (i >= len) {
-                                response.write(JSON.stringify(docList));
-                                console.log('ending response...');
-                                response.end();
-                            }
-                        } else {
-                            console.log(err);
-                        }
-                    });
-
-                });
-            }
-
-        } else {
-            console.log(err);
-        }
-    });
-
-});
-
-
-http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+module.exports = get_ws();
+app.set('ws', ws); */
+module.exports =  app ;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,135 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var userlogin;
+var pwd ;
+var Senr;
+/* GET home page. */
+var tagline;
+var app = require('../app');
 
-/*
- * GET home page.
- */
+router.get('/', function(req, res, next) {
+	
+  res.render(path.join(__dirname, '../', 'views', 'index.ejs'));
+});
+router.post('/login', function(req, res) {
+	console.log("inside post");
+	//{"username":"sfd","password":"dsa"}
+	userlogin=req.body.username;
+	pwd = req.body.password;
+	console.log("user "+userlogin+" "+"password "+pwd);
+	
+	tagline = "Please enter some data for your Company employee.";
+    switch(userlogin){
+	case 'skat':
+		Senr=20170234;
+		break;
+	case 'virksomhed':
+		Senr='20071246';
+		break;
+	case 'kontrol':
+		Senr='10006547';
+		break;
+		
+}
+	
+	
+	if(userlogin==='skat'|| userlogin==='virksomhed'){
+		console.log(' skat virksomhed user ');
+		
+	/*var obj = 	{
+				type: 'get',
+				//name: marbleName,
+				//user: user,
+				v: 1
+			};*/
+	
+	/*var obj = 	{
+			type: 'addToLogBog',
+			cprNum:'1000',
+			VirkNum:'1010',
+			cprNavn:'Jyoti Gover',
+			DOW:"20102016",
+			NoOFHours:'10',
+			v: 1
+		};
+	sendMsg(req,obj);*/
+	//sendMsg(req,JSON.stringify({type: 'get', v:1}));
+	res.render(path.join(__dirname, '../', 'views', 'logEntry.ejs'), {
+        user: userlogin,
+        tagline: tagline,
+        senr: Senr
+    });
+	}else if(userlogin==='kontrol'){
+		res.render(path.join(__dirname, '../', 'views', 'logQuery.ejs'), {
+	        user: userlogin,
+	        tagline: tagline,
+	        senr: Senr
+	    });
+	}else 
+		res.render(path.join(__dirname, '../', 'views', 'error.ejs'));
+	
+});
 
-exports.index = function(req, res){
-  res.render('index.html', { title: 'Cloudant Boiler Plate' });
-};
+//send a message, socket might be closed...
+/*function sendMsg(req, json){
+	console.log('inside AngulargJS sendMsg');
+	console.log('printing ws');
+	console.log('app.get(ws)' + req.app.get('ws'));
+	if(req.app.get('ws')){
+		try{
+			req.app.get('ws').send(JSON.stringify(json));
+		}
+		catch(e){
+			console.log('[ws error] could not send msg', e);
+		}
+	}
+}*/
+router.post('/logEntry', function(req, res) {
+	
+	console.log("trying to print log entry body"+req.body.personalDetail);
+	console.log("trying new" + JSON.stringify(req.body.personalDetail));
+	console.log(JSON.stringify(req.body));
+	var obj = 	{
+			type: 'addToLogBog',
+			cprNum:'1002',
+			VirkNum:'1011',
+			cprNavn:'vishal',
+			DOW:"20102006",
+			NoOFHours:'10',
+			v: 1
+		};
+	//sendMsg(req,obj);
+	res.render(path.join(__dirname, '../', 'views', 'logQuery.ejs'), {
+        user: userlogin,
+        tagline: tagline,
+        senr: Senr
+    });
+});
+
+router.get('/error', function(req, res, next) {
+	console.log("username and password"+req.body);
+  res.render(path.join(__dirname, '../', 'views', 'error.ejs'));
+});
+
+router.get('/title',function(req,res){
+	res.json([{ title: 'LogEntry Page' }]);
+});
+
+router.get('/data', function(req,res){
+	res.json([{"CPRNum":1002,"VirkNum":1011,"CPRNavn":"vishal","DOW":"20102006","NoOfHours":10,"Comments":""},
+		{"CPRNum":1002,"VirkNum":1011,"CPRNavn":"vishal","DOW":"20102006","NoOfHours":10,"Comments":""},
+		{"CPRNum":1002,"VirkNum":1011,"CPRNavn":"vishal","DOW":"20102006","NoOfHours":10,"Comments":""}]); 
+	
+	/*res.json([{"cpr": 9899553461, "navn": "Helene", "date": "20160101", "duration": "8"},
+		{"cpr": 3567432178, "navn": "Helene", "date": "20160101", "duration": "8"},
+		{"cpr": 3267894531, "navn": "Ola", "date": "20160101", "duration": "8"},
+		{"cpr": 116743289, "navn": "Barito", "date": "20160101", "duration": "8"},
+		{"cpr": 5643217843, "navn": "Mads", "date": "20160101", "duration": "8"},
+		{"cpr": 3215674396, "navn": "Orion", "date": "20160101", "duration": "8"},
+		{"cpr": 3761239075, "navn": "Palmes", "date": "20160101", "duration": "8"},
+        {"cpr": 9056342901, "navn": "Yena", "date": "20160101", "duration": "8"},
+        {"cpr": 8954321678, "navn": "Peter", "date": "20160101", "duration": "8"},
+        {"cpr": 8764316789, "navn": "Santa", "date": "20160101", "duration": "8"}]);*/
+});
+module.exports = router;
